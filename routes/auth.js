15e68be7ae16853c3a49c6466217620e955acd41
AlexMkk <asmkk@mail.ru>
2021-02-23 21:45:08 +0300

    async validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const router = Router()
 const bcrypt = require('bcryptjs') // шифрование пароля
 const User = require('../models/user')
+const { registerValidators } = require('../utils/validators')
+const { validationResult } = require('express-validator/check')
 
 router.get('/login', (req, res) => {
   res.render('auth/login', {
@@ -20,9 +22,9 @@ router.get('/logout', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const {email, password} = req.body
+    const { email, password } = req.body
     const candidate = await User.findOne({ email }) // при авторизации ищем пользователя по email
-    
+
     if (candidate) {
       const areSame = await bcrypt.compare(password, candidate.password) // проверяем пароль из формы с паролем из бд
       if (areSame) {
@@ -30,7 +32,7 @@ router.post('/login', async (req, res) => {
         req.session.user = user
         req.session.isAuthenticated = true
         req.session.save(err => { // вызываем метод save, чтобы не было редиректа раньше привязки сессий
-          if(err) {
+          if (err) {
             throw err
           } else {
             res.redirect('/')
@@ -38,7 +40,7 @@ router.post('/login', async (req, res) => {
         })
       } else {
         req.flash('loginError', 'Неверный пароль')
-        res.redirect('/auth/login#login')  
+        res.redirect('/auth/login#login')
       }
     } else {
       req.flash('loginError', 'Такого пользователя не существует')
@@ -47,27 +49,28 @@ router.post('/login', async (req, res) => {
   } catch (e) {
     console.log(e)
   }
-  
-  
+
+
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => { // body - валидация полей
   try {
-    const {email, password, repeat, name} = req.body // получаем поля формы
-    const candidate = await User.findOne({ email }) // проверяем по email зарегистрирован ли такой пользователь
+    const { email, password, name } = req.body // получаем поля формы
 
-    if (candidate) {
-      req.flash('registerError', 'Пользователь с таким email уже существует')
-      res.redirect('/auth/login#register')
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10)
-      const user = new User({
-        email, name, password: hashPassword, cart: {items: []}
-      })
-      await user.save()
-      res.redirect('/auth/login#login')
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) { // если есть ошибки при валидации
+
+      req.flash('registerError', errors.errors[0].msg)
+      return res.status(422).redirect('/auth/login#register') // возвращаем статус ошибки
     }
-    
+
+    const hashPassword = await bcrypt.hash(password, 10)
+    const user = new User({
+      email, name, password: hashPassword, cart: { items: [] }
+    })
+    await user.save()
+    res.redirect('/auth/login#login')
   } catch (e) {
     console.log(e)
   }
